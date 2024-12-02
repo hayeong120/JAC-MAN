e03219afa049078e499d32d326b5e6c8f28b77b1
hayoung <jbg01200249@gmail.com>
2024-12-03 02:37:48 +0900

    ✨[feat] 플레이어 구현

---
+++ player.cpp
@@ -0,0 +1,83 @@
+#include "Player.h"
+
+Player::Player(int x, int y, const char* img, COLOR color) : x(x), y(y), color(color)
+{
+	state = STOP;
+	setImg(img);
+	score = 0;
+	Buff = false;
+	isDie = false;
+}
+
+Player::~Player() {}
+
+void Player::Input()
+{
+	state = STOP;
+	if (GetAsyncKeyState(VK_UP) & 0x8000)
+		state = UP;
+	else if (GetAsyncKeyState(VK_DOWN) & 0x8000)
+		state = DOWN;
+	else if (GetAsyncKeyState(VK_LEFT) & 0x8000)
+		state = LEFT;
+	else if (GetAsyncKeyState(VK_RIGHT) & 0x8000)
+		state = RIGHT;
+}
+
+void Player::Update(Map* map)
+{
+	switch (state) {
+	case UP:
+		y -= 1;
+		if (map->getMapInfo(x, y) == WALL) y += 1;
+		break;
+	case DOWN:
+		y += 1;
+		if (map->getMapInfo(x, y) == WALL) y -= 1;
+		break;
+	case LEFT:
+		x -= 1;
+		if (x < 0) x = map_width - 1;
+		else if (map->getMapInfo(x, y) == WALL) x += 1;
+		break;
+	case RIGHT:
+		x += 1;
+		if (x == map_width) x = 0;
+		else if (map->getMapInfo(x, y) == WALL) x -= 1;
+		break;
+	}
+
+	switch (map->GetItem(x, y)) {
+	case _POINT: Beep(1000, 20); score += 10; break;
+	case ITEM: Beep(1000, 20);	getItem(); break;
+	}
+
+	if (clock() - BuffStart > 5000) {
+		Buff = false;
+	}
+
+	map->setBuffer(x, y, PLAYER);
+
+}
+
+void Player::Render() {
+	gotoxy(4, 31);
+	setColor(White);
+	if (Buff == true) printf("BUFF  ¡º  ON  ¡»");
+	else printf("BUFF  ¡º  OFF ¡»");
+
+	gotoxy(4, 32);
+	puts("                  ");
+	gotoxy(4, 32);
+
+
+	gotoxy(30, 31);
+	setColor(White);
+	printf("SCORE ¡º %04d ¡» ", score);
+}
+
+void Player::getItem()
+{
+	BuffStart = clock();
+	Buff = true;
+}
---
+++ player.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "Util.h"
+#include"Map.h"
+
+class Player
+{
+public:
+	Player(int x, int y, const char* img, COLOR color);
+	~Player();
+
+public:
+	void Input();				// 사용자 입력을 받는 함수
+	void Update(Map* map);		// 플레이어 상태를 업데이트하는 함수
+	void Render();				// 플레이어를 렌더링하는 함수
+
+	void getItem();				// 아이템을 얻을 때 호출되는 함수
+
+private:
+	int x, y;				// 플레이어의 위치
+	char img[3];			// 플레이어 이미지 (문자형태로 저장)
+	STATE state;			// 플레이어의 상태 (이동 방향 등)
+	int score;				// 플레이어의 점수
+	COLOR color;			// 플레이어의 색상
+
+	bool Buff;			// 플레이어가 버프 상태 여부
+	int BuffStart;		// 버프 시작 시간
+
+	bool isDie;			// 플레이어가 죽었는지 여부
+
+
+public:
+	// getter와 setter 함수들
+	void setX(int x) { this->x = x; }
+	void setY(int y) { this->y = y; }
+	void setImg(const char* img) { strcpy_s(this->img, img); }
+	void setState(STATE state) { this->state = state; }
+	void setIsDie(bool temp) { this->isDie = temp; }
+
+	int getX() { return x; }
+	int getY() { return y; }
+	const char* getImg() { return img; }
+	int getState() { return state; }
+	int getScore() { return score; }
+	bool isDied() { return isDie; }
+	bool isBuff() { return Buff; }
+};
